dde2538d91b50bf1f5574f3c6e010f3123e41a17
mibrahimy <ebraheem@outlook.com>
2024-09-03 12:45:54 +0500

    show Alert when button clicked

---
+++ src/Alert.jsx
@@ -0,0 +1,12 @@
+const Alert = () => {
+    return (
+
+        <div className="alert alert-primary" role="alert">
+          A simple primary alert—check it out!
+        </div>
+
+            );
+  };
+  
+  export default Alert;
+  
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 
 import Button from "./Button"
-
-function onClick(){
- console.log(`Button  clicked`)
-}
-
+import { useState } from 'react';
+import Alert from "./Alert"
 function App() {
+  const [showAlert, setShowAlert] = useState(false);
 
+  function handleClick() {
+    setShowAlert(true);
+    
+    setTimeout(() => setShowAlert(false), 3000);
+  }
   return (
     <>
-<Button color='btn-primary' text='this is my button' onClick={onClick}></Button>
-<Button color='btn-danger' text='this is my second button' onClick={onClick}></Button>
-
+      <Button color='btn-primary' text='this is my button' onClick={handleClick} />
+      <Button color='btn-danger' text='this is my 2 button' onClick={handleClick} />
+      {showAlert && <Alert/>}
     </>
-
   );
 }
 
-export default App
+export default App;
